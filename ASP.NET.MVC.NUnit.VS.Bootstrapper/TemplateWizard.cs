0f66e93a01044f66b5b02a852ae7836303e1555b
Marcus Kimpenhaus <kimpenhaus@devil-engineering.de>
2012-12-05 17:29:14 +0100

    added bootstrapper for different VS versions

---
+++ ASP.NET.MVC.NUnit.VS.Bootstrapper/TemplateWizard.cs
@@ -0,0 +1,43 @@
+﻿namespace ASP.NET.MVC.NUnit.VS.Bootstrapper
+{
+    using Microsoft.VisualStudio.TemplateWizard;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    
+    public sealed class TemplateWizard
+        : IWizard
+    {
+        public void BeforeOpeningFile(EnvDTE.ProjectItem projectItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ProjectFinishedGenerating(EnvDTE.Project project)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ProjectItemFinishedGenerating(EnvDTE.ProjectItem projectItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RunFinished()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ShouldAddProjectItem(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
